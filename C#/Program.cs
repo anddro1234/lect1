8ed744f051f2cb9f84a20d392696be862cc1b564
anddro1234 <andriaarchvadze989@gmail.com>
2026-05-30 17:35:19 +0400

    Add project files.

---
+++ C#/Program.cs
@@ -0,0 +1,90 @@
+﻿namespace C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* davaleba1
+            Console.WriteLine("seiyvane seni asaki:");
+            string age = Console.ReadLine();
+
+              bool agee = byte.TryParse(age, out byte b);
+
+
+
+              if (b >= 18)
+              {
+                  Console.WriteLine("dawere gancxadeba");
+                  string xma = Console.ReadLine();
+                  if (xma != null)
+                  {
+                      Console.WriteLine("adloba xmis micemistvis: " + xma);
+                  }else
+                  {
+                      Console.WriteLine("amwuxarod xmis micema ver moxerxda");
+                  }
+
+              }else
+              {
+                  Console.WriteLine("arasrulwlovani xar");
+              } */
+            /*   davaleba2
+            Console.WriteLine("dawere sami ricxvi rata gaigo udidesi:");
+          
+            string numm1 = Console.ReadLine();
+            string numm2 = Console.ReadLine();
+            string numm3 = Console.ReadLine();
+
+            bool num1 = int.TryParse(numm1, out int a);
+            bool num2 = int.TryParse(numm2, out int b);
+            bool num3 = int.TryParse(numm3, out int c);
+
+            int biggestnum = 0;
+
+            if (a > b && a > c)
+            {
+                biggestnum = a;
+                Console.WriteLine("didi ricxvia:" + a);
+            }
+            else if (b > a && b > c)
+            {
+                biggestnum = b;
+                Console.WriteLine("didi ricxvia:" + b);
+            }
+            else if (c > a && c > b)
+            {
+                biggestnum = c;
+                Console.WriteLine("didi ricxvia:" + c);
+            }
+            else
+            {
+                Console.WriteLine("samwuxaroa");
+            } */
+            /* davaleba3
+
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
+
+            bool numm11 = int.TryParse(num1, out int num);
+            bool numm22 = int.TryParse(num2, out int numu);
+
+           
+
+
+
+            if (num == numu)
+            {
+                int sum1 = (num + numu) * 3;
+                Console.WriteLine("(ertnair ricxvze sammagdeba) seni ricxvi aris: " + sum1);
+            }
+            else if (numm11 && numm22) { 
+                int sum2 = num + numu;
+                Console.WriteLine("seni ricxvi aris:" + sum2);
+            }
+            else
+            {
+                Console.WriteLine("samwuxarod ver moxerxda");
+            }  */
+        }
+    }
+}
---
+++ lect3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace lect3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
