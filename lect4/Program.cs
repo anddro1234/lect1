4585a4929ed85b15606c1f5f8db62770676cc85e
anddro1234 <andriaarchvadze989@gmail.com>
2026-06-04 12:56:47 +0400

    lekcia4

---
+++ lect4/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace lect4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* davaleba 1
+
+             string tab = Console.ReadLine();
+
+             bool hh = int.TryParse(tab, out int a);
+             if (hh)
+             {
+                 for (int j = 1; j <= 10; j++)
+                 {
+                   int  f = a * j;
+                     Console.WriteLine($"{a}" + "*" + $"{j}" + "=" + $"{f}");
+                 }
+                 Console.WriteLine();
+             }
+             else
+             {
+                 Console.WriteLine("gtxovt seiyvane ricxvi");
+             }
+            */
+            /*   davaleba 2
+
+             string seynum = Console.ReadLine();
+
+             bool num = int.TryParse(seynum, out int an);
+
+             int height = an;
+
+             for (int i = 1; i <= height; i++)
+             {
+                 string line = "";
+                 for (int j = 1; j <= height - i; j++)
+                 {
+                     line = line + " ";
+                 }
+                 for (int k = 1; k <= (2 * i - 1); k++)
+                 {
+                     line = line + "*";
+                 }
+                 Console.WriteLine(line);
+             } */
+            /* davaleba 3
+            string num = Console.ReadLine();    
+            bool num1 = int.TryParse(num, out int a);
+
+            if (num1)
+            {
+              int sum = 0;
+
+              for (int i = 0; i <= a; i++)
+            {
+                    if (i % 2 == 0)
+                    {
+                        sum += i;
+                    }
+                }  
+              Console.WriteLine($"1-dan {a}-mde luwi ricxvis jami: {sum}");
+            }
+            
+            else
+            {
+                Console.WriteLine("seiyvane ricxvi");
+            }*/
+            /* davaleba 4
+
+            Console.WriteLine("es aris seni num");
+            Random random = new();
+            int rann = random.Next(1000, 9999);
+            Console.WriteLine(rann);
+            int v;
+            do{
+
+            Console.WriteLine("seiyvane num");
+            string verify = Console.ReadLine();
+            bool veriffy = int.TryParse(verify, out v); 
+            
+               if (rann == v)
+            {
+                Console.WriteLine("verified sucsesful");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("verification failed");
+                   
+
+
+            }  
+            }
+            while (true); */
+        }
+    }
+}
