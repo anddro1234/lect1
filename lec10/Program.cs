468660e5daf53e5296b5e6c6dc8a8a94932c8bf9
anddro1234 <andriaarchvadze989@gmail.com>
2026-06-26 10:59:27 +0400

    lect10

---
+++ lec10/Program.cs
@@ -0,0 +1,46 @@
+﻿using lec10.classes;
+
+namespace lec10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employ[] employ = new Employ[8];
+
+            employ[0] = new Employ("andria", "archvadze", new DateTime(1990, 5, 15), Country.georgia, Gender.male, Contacts.andria);
+            employ[1] = new Employ("nika", "lursmanasvili", new DateTime(1988, 7, 18), Country.georgia, Gender.male, Contacts.nika);
+            employ[2] = new Employ("lika", "tartarasvili", new DateTime(1990, 3, 30), Country.georgia, Gender.female, Contacts.vigac);
+            employ[3] = new Employ("jhon", "doe", new DateTime(1990, 6, 5), Country.usa, Gender.male, Contacts.vigac);
+            employ[4] = new Employ("lee", "hu", new DateTime(1990, 5, 9), Country.japan, Gender.male, Contacts.vigac);
+            employ[5] = new Employ("james", "lebron", new DateTime(1990, 9, 7), Country.england, Gender.male, Contacts.vigac);
+            employ[6] = new Employ("guks", "vigacaze", new DateTime(1990, 5, 5), Country.georgia, Gender.male, Contacts.vigac);
+            employ[7] = new Employ("jordan", "michael", new DateTime(1990, 12, 22), Country.usa, Gender.male, Contacts.vigac);
+
+           Employ.Printemploycont(employ, Country.georgia);
+           Employ.Printemploycont(employ, Country.usa);
+           Employ.Printemploycont(employ, Country.japan);
+           Employ.Printemploycont(employ, Country.england);
+
+        }
+    }
+    public enum Country
+    {
+         georgia,
+         usa,
+         japan,
+         england,
+
+    }
+    public enum Gender
+    {
+        male,
+        female,
+    }
+    public enum Contacts
+    {
+        andria = 555112233,
+        nika = 555445566,
+        vigac = 555778899,
+    }
+}
---
+++ lec10/classes/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lec10.classes
+{
+    internal class Employ
+    {
+        public string name;
+        public string surename;
+        public DateTime datetime;
+        public Country country;
+        public Gender gender;
+        public Contacts contact;
+        public Employ(string name, string surename, DateTime datetime, Country country, Gender gender, Contacts contact)
+        {
+            this.name = name;
+            this.surename = surename;
+            this.datetime = datetime;
+            this.country = country;
+            this.gender = gender;
+            this.contact = contact;
+        }
+        public int getage()
+        {
+            DateTime today = DateTime.Now;
+            int age = today.Year - this.datetime.Year;
+            if (this.datetime.Date > today.AddYears(-age))
+            {
+                age--;
+            }
+            return age;
+        }
+
+
+        public static void Printemploycont(Employ[] employs , Country country) {
+            Console.WriteLine("tanamsromlebi qveynis mixedvit " + country);
+            foreach (var item in employs)
+            {
+                if (item.country == country)
+                {
+                    Console.WriteLine(item.name + " " + item.surename);
+                }
+            }
+        }
+    }
+}
